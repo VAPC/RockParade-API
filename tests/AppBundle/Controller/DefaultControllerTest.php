89d88484f6a1dc89e0413caba53fa5bc1a6fb435
Petr Karmashev <smonkl@bk.ru>
2016-05-24 14:49:49 +0300

    DefaultControllerTest refactored

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,17 +3,20 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
 
-/** {@inheritDoc} */
+/**
+ * @author Vehsamrak
+ */
 class DefaultControllerTest extends WebTestCase
 {
 
     /** @test */
     public function indexAction_indexPage_greetingsPhraseRenderedInResponse()
     {
-        $client = static::createClient();
+        $client = $this->createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request(Request::METHOD_GET, '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Rock Parade!', $crawler->filter('h3')->text());
