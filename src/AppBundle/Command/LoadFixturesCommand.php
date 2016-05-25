a42ee6dab98435acec713df2985944de2a4bfe5c
Petr Karmashev <smonkl@bk.ru>
2016-05-26 01:32:11 +0300

    LoadFixturesCommand introduced

---
+++ src/AppBundle/Command/LoadFixturesCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Fixture\RoleFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/** {@inheritDoc} */
+class LoadFixturesCommand extends ContainerAwareCommand
+{
+
+    /** {@inheritDoc} */
+    protected function configure()
+    {
+        $this->setName('rock:fixture:load');
+        $this->setDescription('Load fixtures to database');
+    }
+
+    /** {@inheritDoc} */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $doctrineService = $this->getContainer()->get('doctrine');
+        $entityManager = $doctrineService->getManager();
+
+        $fixtures = [
+            new RoleFixture(),
+        ];
+
+        foreach ($fixtures as $fixture) {
+            if ($fixture instanceof FixtureInterface) {
+                $fixture->load($entityManager);
+            }
+        }
+
+        $output->writeln('Fixtures loaded to database.');
+    }
+}
