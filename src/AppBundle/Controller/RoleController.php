d61d2043e4495b2e80bf0925d1abd5e69eaa8229
Petr Karmashev <smonkl@bk.ru>
2016-05-26 13:55:58 +0300

    RoleController::addAction() and functional tests

--- src/AppBundle/Controller/RoleController.php
+++ src/AppBundle/Controller/RoleController.php
@@ -4,10 +4,14 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Role;
 use AppBundle\Controller\Infrastructure\RestController;
+use AppBundle\Entity\RoleRepository;
+use AppBundle\Response\ApiError;
 use AppBundle\Response\ApiResnonse;
+use AppBundle\Response\EmptyApiResponse;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -35,6 +39,57 @@ class RoleController extends RestController
         $roles = $rolesRepository->findAll();
 
         $response = new ApiResnonse($roles, Response::HTTP_OK);
+
+        return $this->respond($response);
+    }
+
+    /**
+     * Add roles to user
+     * @Route("/add", name="roles_add")
+     * @Method("POST")
+     * @ApiDoc(
+     *     section="Roles",
+     *     requirements={
+     *         {
+     *             "name"="login",
+     *             "dataType"="string",
+     *             "requirement"="\w",
+     *             "description"="user login"
+     *         },
+     *         {
+     *             "name"="roles",
+     *             "dataType"="array",
+     *             "requirement"="\w",
+     *             "description"="applicable roles"
+     *         }
+     *     },
+     *     statusCodes={
+     *         200="Roles were assigned to user",
+     *         400="Mandatory parameters are missed or not all provided roles are valid",
+     *     }
+     * )
+     * @param Request $request
+     * @return Response
+     */
+    public function addAction(Request $request): Response
+    {
+        $userLogin = filter_var($request->request->get('login'), FILTER_SANITIZE_STRING) ?: '';
+        $roleNames = (array) $request->request->get('roles');
+
+        if ($userLogin && $roleNames) {
+            /** @var RoleRepository $rolesRepository */
+            $rolesRepository = $this->getDoctrine()->getRepository(Role::class);
+
+            if (count($roleNames) === $rolesRepository->countRolesWithNames($roleNames)) {
+                $response = new EmptyApiResponse(Response::HTTP_OK);
+            } else {
+                $response = new ApiError('Not all provided roles are valid.', Response::HTTP_BAD_REQUEST);
+            }
+
+        } else {
+            $response = new ApiError('Properties "login" and "roles" are mandatory.', Response::HTTP_BAD_REQUEST);
+        }
+
 
         return $this->respond($response);
     }
--- tests/AppBundle/Controller/RoleControllerTest.php
+++ tests/AppBundle/Controller/RoleControllerTest.php
@@ -11,6 +11,10 @@ use Tests\FunctionalTester;
 class RoleControllerTest extends FunctionalTester
 {
 
+    const USER_LOGIN_FIRST = 'first';
+    const ROLE_ADMIN = 'admin';
+    const ROLE_NONEXISTENT = 'nonexistent';
+
     /** {@inheritDoc} */
     protected function setUp()
     {
@@ -34,5 +38,62 @@ class RoleControllerTest extends FunctionalTester
         $this->assertEquals(200, $responseCode);
         $this->assertEquals('admin', $contents['data'][0]['name']);
         $this->assertEquals('Администратор', $contents['data'][0]['description']);
+    }
+
+    /** @test */
+    public function addAction_POSTRolesAddRequestWithLoginAndRoles_response200()
+    {
+        $parameters = $this->createParametersLoginAndRoles();
+
+        $this->sendPostRequest('/roles/add', $parameters);
+        $responseCode = $this->getResponseCode();
+
+        $this->assertEquals(200, $responseCode);
+    }
+
+    /** @test */
+    public function addAction_POSTRolesAddRequestWithEmptyParameters_response400AndErrorMessage()
+    {
+        $this->sendPostRequest('/roles/add', []);
+        $contents = $this->getResponseContents();
+        $responseCode = $this->getResponseCode();
+
+        $this->assertEquals(400, $responseCode);
+        $this->assertEquals('Properties "login" and "roles" are mandatory.', $contents['error']);
+    }
+
+    /** @test */
+    public function addAction_POSTRolesAddRequestWithLoginAndNonexistentRole_response400AndErrorMessage()
+    {
+        $parameters = $this->createParametersLoginAndNonexistentRoles();
+
+        $this->sendPostRequest('/roles/add', $parameters);
+        $contents = $this->getResponseContents();
+        $responseCode = $this->getResponseCode();
+
+        $this->assertEquals(400, $responseCode);
+        $this->assertEquals('Not all provided roles are valid.', $contents['error']);
+    }
+
+    /**
+     * @return array
+     */
+    private function createParametersLoginAndRoles(): array
+    {
+        return [
+            'login' => self::USER_LOGIN_FIRST,
+            'roles' => [self::ROLE_ADMIN],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function createParametersLoginAndNonexistentRoles(): array
+    {
+        $parameters = $this->createParametersLoginAndRoles();
+        $parameters['roles'][] = self::ROLE_NONEXISTENT;
+
+        return $parameters;
     }
 }
