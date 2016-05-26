f9f9ffa6871f9e43f639b02b1048b1045b4084a5
Petr Karmashev <smonkl@bk.ru>
2016-05-27 01:27:19 +0300

    Band and Organizer entities introduced

---
+++ src/AppBundle/Entity/Organizer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Organizer
+ * @ORM\Table(name="organizers")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\OrganizerRepository")
+ */
+class Organizer
+{
+
+    /**
+     * @var string
+     * @ORM\Id
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="registration_date", type="datetime")
+     */
+    protected $registrationDate;
+
+    /**
+     * @var string
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    protected $description;
+
+    /**
+     * @var User[]|ArrayCollection
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\User", cascade={"persist", "remove"})
+     * @ORM\JoinTable(name="users_organizers",
+     *      joinColumns={@ORM\JoinColumn(name="community_name", referencedColumnName="name")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="user_login", referencedColumnName="login")}
+     *      )
+     */
+    protected $users;
+
+    /**
+     * @param string $name
+     * @param string $description
+     */
+    public function __construct(string $name, string $description)
+    {
+        $this->registrationDate = new \DateTime();
+        $this->name = $name;
+        $this->description = $description;
+        $this->users = new ArrayCollection();
+    }
+}
+
---
+++ src/AppBundle/Entity/OrganizerRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/** {@inheritDoc} */
+class OrganizerRepository extends EntityRepository {}
