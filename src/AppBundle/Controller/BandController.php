8d0129374b9d10a56d42008e6f940ac89d80c4dd
Petr Karmashev <smonkl@bk.ru>
2016-05-31 00:13:57 +0300

    BandController::createAction() implemented

--- src/AppBundle/Controller/BandController.php
+++ src/AppBundle/Controller/BandController.php
@@ -2,12 +2,19 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\Form\Form;
+use Symfony\Component\Validator\Constraints as Assert;
 use AppBundle\Controller\Infrastructure\RestController;
 use AppBundle\Entity\Band;
+use AppBundle\Response\ApiError;
 use AppBundle\Response\ApiResnonse;
+use AppBundle\Response\EmptyApiResponse;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -34,5 +41,74 @@ class BandController extends RestController
         $response = new ApiResnonse($bandRepository->findAll(), Response::HTTP_OK);
 
         return $this->respond($response);
+    }
+
+    /**
+     * Create new band
+     * @Route("/create", name="band_create")
+     * @Method("POST")
+     * @ApiDoc(
+     *     section="Band",
+     *     requirements={
+     *         {
+     *             "name"="name",
+     *             "dataType"="string",
+     *             "requirement"="true",
+     *             "description"="band name"
+     *         },
+     *         {
+     *             "name"="description",
+     *             "dataType"="string",
+     *             "requirement"="true",
+     *             "description"="band description"
+     *         },
+     *         {
+     *             "name"="users",
+     *             "dataType"="array",
+     *             "requirement"="",
+     *             "description"="band users"
+     *         },
+     *     },
+     *     statusCodes={
+     *         200="New band was created",
+     *         400="Validation error",
+     *     }
+     * )
+     * @param Request $request
+     * @return Response
+     */
+    public function createAction(Request $request): Response
+    {
+        $form = $this->createBandCreateForm();
+        $this->processForm($request, $form);
+
+        if ($form->isValid()) {
+            $response = new EmptyApiResponse(Response::HTTP_OK);
+        } else {
+            $response = new ApiError($this->getFormErrors($form), Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->respond($response);
+    }
+
+    /**
+     * @return Form
+     */
+    private function createBandCreateForm()
+    {
+        $formBuilder = $this->createFormBuilder(
+            new class
+            {
+                /** @Assert\NotBlank(message="Parameter 'name' is mandatory") */
+                public $name;
+
+                /** @Assert\NotBlank(message="Parameter 'description' is mandatory") */
+                public $description;
+            }
+        )
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class);
+
+        return $formBuilder->getForm();
     }
 }
