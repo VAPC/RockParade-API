21bc3d0e71dced61db68e3f7bfb44189ac5f51fa
Petr Karmashev <smonkl@bk.ru>
2016-05-31 22:15:23 +0300

    UserNotFound exception message

--- src/AppBundle/Exception/UserNotFound.php
+++ src/AppBundle/Exception/UserNotFound.php
@@ -8,4 +8,11 @@ namespace AppBundle\Exception;
 class UserNotFound extends \DomainException
 {
 
+    /**
+     * @param string $userLogin
+     */
+    public function __construct(string $userLogin = null)
+    {
+        parent::__construct(sprintf('User %s was not found.', $userLogin));
+    }
 }
