7c170fd639ef18963ca3306a2e23a46bbefcf9a4
Petr Karmashev <smonkl@bk.ru>
2016-05-31 21:45:20 +0300

    BandFixture added

---
+++ src/AppBundle/Fixture/BandFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Fixture;
+
+use AppBundle\Entity\Band;
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * @author Vehsamrak
+ */
+class BandFixture implements FixtureInterface
+{
+
+    /** {@inheritDoc} */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User('bander', 'Bander');
+
+        $entities = [
+            $user,
+            new Band('Banders', [$user], 'Band description.'),
+        ];
+
+        foreach ($entities as $entity) {
+            $manager->persist($entity);
+        }
+
+        $manager->flush();
+    }
+}
