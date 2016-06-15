f738b8f5b919105372623af7c97c0c7cb0232883
Petr Karmashev <smonkl@bk.ru>
2016-06-15 21:54:33 +0300

    BandMemberRepository::findByBandName() method

--- src/AppBundle/Entity/Repository/BandMemberRepository.php
+++ src/AppBundle/Entity/Repository/BandMemberRepository.php
@@ -2,10 +2,23 @@
 
 namespace AppBundle\Entity\Repository;
 
+use AppBundle\Entity\BandMember;
 use Doctrine\ORM\EntityRepository;
 
 /** {@inheritDoc} */
 class BandMemberRepository extends EntityRepository
 {
 
+    /**
+     * @return BandMember[]
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findByBandName(string $name): array
+    {
+        $queryBuilder = $this->createQueryBuilder('band_member');
+        $queryBuilder->where('band_member.band = :bandName');
+        $queryBuilder->setParameter('bandName', $name);
+        
+        return $queryBuilder->getQuery()->getArrayResult();
+    }
 }
