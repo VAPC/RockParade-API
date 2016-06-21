cf05896d5919c163ba87d9d79ec88b2d25a2a297
Petr Karmashev <smonkl@bk.ru>
2016-06-22 00:52:00 +0300

    Permission fix to composer inside docker container

--- app/docker/init.sh
+++ app/docker/init.sh
@@ -2,6 +2,8 @@
 
 echo "Setting up permissions to www-data ..."
 usermod -u 1000 www-data
+sudo chown -R www-data /vagrant/vendor
+sudo chown -R www-data /vagrant/bin
 
 echo "Starting PHP-fpm ..."
 service php7.1-fpm start
@@ -11,7 +13,7 @@ service mysql start
 
 echo "Installing composer dependencies ..."
 cd /vagrant
-php /composer.phar install
+sudo -u www-data php /composer.phar install
 
 echo "Starting Nginx ..."
 nginx
