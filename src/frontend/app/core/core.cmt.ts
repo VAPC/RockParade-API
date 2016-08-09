cbef5d3a6fbd4834e5a44e3390321698671fca62
Daiver <daiver@wowworks.ru>
2016-08-10 00:06:37 +0300

    add dependencies for 'gulp vendor'

--- src/frontend/app/core/core.cmt.ts
+++ src/frontend/app/core/core.cmt.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {EventsComponent} from '../events/events.cmt';
 
 @Component({
     selector: 'rock-parade',
-    template: '<div class="core">Fuck off rock parade!!!</div>'
+    template: `<div><div class="core">Fuck off rock parade!!!</div>
+                <events-list>Loading...</events-list></div>`,
+    directives: [EventsComponent]
 })
-export class CoreComponent { }
+export class CoreComponent {
+}
