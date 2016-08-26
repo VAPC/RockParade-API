58646990d20c80635326b2d72bae344e629203b4
Petr Karmashev <smonkl@bk.ru>
2016-08-27 01:47:25 +0300

    Creating user and list all users methods removed from api

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -21,28 +21,6 @@ class UserController extends RestController
 {
 
     /**
-     * List all registered users
-     * @Route("s/", name="users_list")
-     * @Method("GET")
-     * @ApiDoc(
-     *     section="User",
-     *     statusCodes={
-     *         200="OK",
-     *     }
-     * )
-     */
-    public function listAction(): Response
-    {
-        /** @var UserRepository $userRepository */
-        $userRepository = $this->get('rockparade.user_repository');
-        $allUsers = $userRepository->findAll();
-
-        $response = new ApiResponse($allUsers, Response::HTTP_OK);
-
-        return $this->respond($response);
-    }
-
-    /**
      * View single user by login
      * @Route("/{login}", name="user_view")
      * @Method("GET")
@@ -68,79 +46,6 @@ class UserController extends RestController
         }
 
         return $this->respond($response);
-    }
-
-    /**
-     * Create new user with given login, name and description
-     * @Route("", name="user_create")
-     * @Method("POST")
-     * @ApiDoc(
-     *     section="User",
-     *     requirements={
-     *         {
-     *             "name"="login",
-     *             "dataType"="string",
-     *             "requirement"="\w",
-     *             "description"="user login in single word"
-     *         },
-     *         {
-     *             "name"="name",
-     *             "dataType"="string",
-     *             "requirement"="[\w\s]",
-     *             "description"="user full name"
-     *         },
-     *         {
-     *             "name"="description",
-     *             "dataType"="string",
-     *             "requirement"=".",
-     *             "description"="user description"
-     *         },
-     *     },
-     *     statusCodes={
-     *         201="New user was created",
-     *         400="Mandatory parameters are missed",
-     *         409="User with given login or username already exists",
-     *     }
-     * )
-     */
-    public function createAction(Request $request): Response
-    {
-        $userLogin = (string) $request->request->getAlnum('login');
-        $userName = $request->request->get('name');
-        $description = $request->request->get('description');
-
-        if (empty($userLogin) || empty($userName)) {
-            return $this->respond(
-                new ApiError('Properties "login" and "name" are mandatory.', Response::HTTP_BAD_REQUEST)
-            );
-        }
-
-        /** @var UserRepository $userRepository */
-        $userRepository = $this->get('rockparade.user_repository');
-
-        if ($userRepository->findOneByLogin($userLogin)) {
-            $result = $this->createUserExistsErrorResult($userLogin);
-        } elseif ($userRepository->findOneByName($userName)) {
-            $result = $this->createUserExistsErrorResult($userName);
-        } else {
-            $user = new User($userLogin, $userName, $description);
-            $userRepository->persist($user);
-            $userRepository->flush();
-
-            $result = new ApiResponse($user, Response::HTTP_CREATED);
-        }
-
-        return $this->respond($result);
-    }
-
-    private function createUserExistsErrorResult(string $userLoginOrName): ApiError
-    {
-        return new ApiError(
-            sprintf(
-                'User with login or username "%s" already exists.',
-                $userLoginOrName
-            ), Response::HTTP_CONFLICT
-        );
     }
 
     private function createUserNotFoundErrorResult(string $userLogin): ApiError
