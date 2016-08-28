6407cc6c2050fb72f70d0f7468ead698659eff18
Petr Karmashev <smonkl@bk.ru>
2016-08-28 03:59:49 +0300

    CollectionApiResponse introduced

---
+++ src/AppBundle/Response/CollectionApiResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Response;
+
+/**
+ * @author Vehsamrak
+ */
+class CollectionApiResponse extends ApiResponse
+{
+
+    /** @var int */
+    private $total;
+
+    /** @var int */
+    private $limit;
+
+    /** @var int */
+    private $offset;
+
+    public function __construct($data, int $httpCode, int $total, int $limit = 0, int $offset = 0)
+    {
+        parent::__construct($data, $httpCode);
+
+        $this->total = $total;
+        $this->limit = $limit;
+        $this->offset = $offset;
+    }
+}
