5e6da3f889bdfb6081be34a745f97b018ffbe59b
Petr Karmashev <smonkl@bk.ru>
2016-08-31 00:59:14 +0300

    Requesting Vkontakte authorization token

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -2,8 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Controller\Infrastructure\RestController;
+use AppBundle\Response\ApiResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -12,10 +13,11 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
  * @Route("login")
  * @author Vehsamrak
  */
-class LoginController extends Controller
+class LoginController extends RestController
 {
 
     const VK_AUTHORIZATION_URL = 'https://oauth.vk.com/authorize';
+    const VK_REQUEST_TOKEN_URL = 'https://oauth.vk.com/access_token';
     const VK_DISPLAY_POPUP = 'popup';
     const VK_DISPLAY_PAGE = 'page';
     const VK_RESPONSE_TYPE_CODE = 'code';
@@ -27,7 +29,7 @@ class LoginController extends Controller
     {
         $parameters = [
             'client_id'     => $this->getParameter('vkontakte.client_id'),
-            'redirect_uri'  => $this->generateUrl('login_vk_oauth_callback', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'redirect_uri'  => $this->generateVkCallbackUrl(),
             'display'       => self::VK_DISPLAY_PAGE,
             'response_type' => self::VK_RESPONSE_TYPE_CODE,
             'v'             => $this->getParameter('vkontakte.version'),
@@ -49,10 +51,16 @@ class LoginController extends Controller
      */
     public function vkOAuthCallbackAction(Request $request)
     {
-        var_dump($request->get('code'));
+        $vkAuthorizationCode = $request->get('code');
 
+        $response = new ApiResponse(
+            [
+                'token' => $this->requestTokenByCode($vkAuthorizationCode),
+            ],
+            Response::HTTP_OK
+        );
 
-        return new Response('Success!');
+        return $this->respond($response);
     }
 
     /**
@@ -69,5 +77,28 @@ class LoginController extends Controller
                 'offline',
             ]
         );
+    }
+
+    private function requestTokenByCode(string $vkAuthorizationCode): array
+    {
+        $parameters = [
+            'client_id'     => $this->getParameter('vkontakte.client_id'),
+            'client_secret' => $this->getParameter('vkontakte.client_secret'),
+            'redirect_uri'  => $this->generateVkCallbackUrl(),
+            'code'          => $vkAuthorizationCode,
+        ];
+
+        $vkontakteRequestTokenUrl = sprintf(
+            '%s?%s',
+            self::VK_REQUEST_TOKEN_URL,
+            http_build_query($parameters)
+        );
+
+        return json_decode(file_get_contents($vkontakteRequestTokenUrl), true);
+    }
+
+    private function generateVkCallbackUrl(): string
+    {
+        return $this->generateUrl('login_vk_oauth_callback', [], UrlGeneratorInterface::ABSOLUTE_URL);
     }
 }
