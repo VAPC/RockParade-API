6df3fe5783bfd7ae3fe7e372634cb2f9622680a0
Petr Karmashev <smonkl@bk.ru>
2016-09-04 14:44:17 +0300

    CollectionApiResponse default limit set to 50

--- src/AppBundle/Response/CollectionApiResponse.php
+++ src/AppBundle/Response/CollectionApiResponse.php
@@ -17,7 +17,7 @@ class CollectionApiResponse extends ApiResponse
     /** @var int */
     private $offset;
 
-    public function __construct($data, int $httpCode, int $total, int $limit = 0, int $offset = 0)
+    public function __construct($data, int $httpCode, int $total, int $limit = 50, int $offset = 0)
     {
         parent::__construct($data, $httpCode);
 
