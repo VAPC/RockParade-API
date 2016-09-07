5c401d92bba284cb12248b3070ec4823d1b0127e
Petr Karmashev <smonkl@bk.ru>
2016-09-08 00:49:42 +0300

    Unique url of link

--- src/AppBundle/Entity/Link.php
+++ src/AppBundle/Entity/Link.php
@@ -24,7 +24,7 @@ class Link
 
     /**
      * @var string
-     * @ORM\Column(name="url", type="string", length=255)
+     * @ORM\Column(name="url", type="string", length=255, unique=true)
      */
     private $url;
 
