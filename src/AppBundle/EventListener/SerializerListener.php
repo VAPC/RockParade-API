eb296be4fef081258df2cbed6f2dafcf24ff8674
Petr Karmashev <smonkl@bk.ru>
2016-09-08 02:20:16 +0300

    SerializerListener configuration block refactored

--- src/AppBundle/EventListener/SerializerListener.php
+++ src/AppBundle/EventListener/SerializerListener.php
@@ -5,7 +5,6 @@ namespace AppBundle\EventListener;
 use AppBundle\Entity\Event;
 use AppBundle\Exception\UnsupportedEntityException;
 use AppBundle\Service\Extractor\Extractor;
-use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
 use JMS\Serializer\JsonSerializationVisitor;
 
@@ -26,13 +25,11 @@ class SerializerListener implements SubscribingHandlerInterface
     /** {@inheritDoc} */
     public static function getSubscribingMethods(): array
     {
-        return [
-            [
-                'format'    => 'json',
-                'type'      => Event::class,
-                'method'    => 'extract',
-            ],
+        $entities = [
+            Event::class,
         ];
+
+        return self::createSubscribingConfiguration($entities);
     }
 
     /**
@@ -42,5 +39,16 @@ class SerializerListener implements SubscribingHandlerInterface
     public function extract(JsonSerializationVisitor $visitor, $entity): array
     {
         return $this->extractor->extract($entity);
+    }
+
+    private static function createSubscribingConfiguration(array $entities): array
+    {
+        $config = [];
+
+        foreach ($entities as $entityClassName) {
+            return ['format' => 'json', 'type' => $entityClassName, 'method' => 'extract'];
+        }
+
+        return $config;
     }
 }
