e98c1f7db0d30e68e84590654f6ada57a25d551a
Petr Karmashev <smonkl@bk.ru>
2016-09-09 00:41:37 +0300

    Controller minor refactoring

--- src/AppBundle/Controller/BandController.php
+++ src/AppBundle/Controller/BandController.php
@@ -48,11 +48,7 @@ class BandController extends RestController
     public function listAction($limit = null, $offset = null): Response
     {
         return $this->respond(
-            $this->createCompleteCollectionResponse(
-                $this->get('rockparade.band_repository'),
-                $limit,
-                $offset
-            )
+            $this->createCompleteCollectionResponse($this->get('rockparade.band_repository'), $limit, $offset)
         );
     }
 
