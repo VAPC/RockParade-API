953b579237af44a2f6cbec6249b8be897fab0f42
Petr Karmashev <smonkl@bk.ru>
2016-09-13 17:40:31 +0300

    EntityCreatorInterface added

---
+++ src/AppBundle/Service/Entity/Infrastructure/EntityCreatorInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Service\Entity\Infrastructure;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\AbstractFormType;
+
+/**
+ * @author Vehsamrak
+ */
+interface EntityCreatorInterface
+{
+
+    public function createEntityByFormData(AbstractFormType $formType, User $creator, string $entityClass);
+}
