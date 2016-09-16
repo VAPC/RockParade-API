2df432074714414fb2e96468741693dd1daa99c6
Petr Karmashev <smonkl@bk.ru>
2016-09-17 02:28:31 +0300

    AmbassadorMemberFormType introduced

---
+++ src/AppBundle/Form/Ambassador/AmbassadorMemberFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form\Ambassador;
+
+use AppBundle\Form\AbstractFormType;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @author Vehsamrak
+ */
+class AmbassadorMemberFormType extends AbstractFormType
+{
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Parameter 'login' is mandatory")
+     */
+    public $login;
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Parameter 'short_description' is mandatory")
+     */
+    public $shortDescription;
+
+    /** @var string */
+    public $description;
+
+    public function setShortDescription(string $shortDescription)
+    {
+        $this->shortDescription = $shortDescription;
+    }
+
+    public function getShortDescription()
+    {
+        return $this->shortDescription;
+    }
+}
