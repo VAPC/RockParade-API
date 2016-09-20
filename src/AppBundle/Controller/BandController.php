6e4d97e6ba85ead2d269a8e3915a7531b1c5f47c
Petr Karmashev <smonkl@bk.ru>
2016-09-21 02:18:42 +0300

    Unused use statements removed

--- src/AppBundle/Controller/BandController.php
+++ src/AppBundle/Controller/BandController.php
@@ -21,7 +21,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @author Vehsamrak
